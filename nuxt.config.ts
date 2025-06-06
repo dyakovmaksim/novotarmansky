a053e0bbed6d38b5f93a6545a598f91a4127439a
Zahar <83747187+ZaharrRR@users.noreply.github.com>
2025-06-06 17:00:23 +0500

    base css and fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  css: ["~/assets/style/main.scss"],
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/icon']
-})
+  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/icon", "@nuxt/fonts"],
+});
