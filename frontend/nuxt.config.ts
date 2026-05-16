785fa7ac686710230af3ed5d3cf8b4394a740953
dyakovmaksim <pochta3478@mail.ru>
2026-05-17 03:48:36 +0500

    добавлена бд

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,4 +4,21 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/icon", "@nuxt/fonts"],
+
+  fonts: {
+    providers: {
+      fontsource: false,
+    },
+  },
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          quietDeps: true,
+          silenceDeprecations: ["import"],
+        },
+      },
+    },
+  },
 });
